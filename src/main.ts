9bdd6c6ea2810ff2c5393f36d683d4ce005ee235
asarium <asarium@gmail.com>
2020-05-30 13:33:23 +0200

    More work

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import * as core from "@actions/core";
-import * as exec from "@actions/exec";
 import * as output from "./output";
 
 async function run(): Promise<void> {
 	try {
-		await exec.exec("pip3", ["install", "pyyaml"]);
-
 		output.fileError("Test!", "test_cpp/test2.cpp", 3, 18);
 	} catch (error) {
 		core.setFailed(error.message);
